e41ca8ce727dcf843f7a08b030360b4283e28824
Paskal Stoyanov <kokotekilata@gmail.com>
2022-11-15 20:34:41 +0000

    Regenerate with Dart Data Class Generator v0.5.4

--- ecommerce_app/lib/src/features/cart/domain/cart.dart
+++ ecommerce_app/lib/src/features/cart/domain/cart.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
@@ -15,29 +17,32 @@ class Cart {
   final Map<ProductID, int> items;
 
   Map<String, dynamic> toMap() {
-    return {
+    return <String, dynamic>{
       'items': items,
     };
   }
 
   factory Cart.fromMap(Map<String, dynamic> map) {
     return Cart(
-      Map<ProductID, int>.from(map['items']),
+      Map<ProductID, int>.from(
+        (map['items'] as Map<ProductID, int>),
+      ),
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory Cart.fromJson(String source) => Cart.fromMap(json.decode(source));
+  factory Cart.fromJson(String source) =>
+      Cart.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() => 'Cart(items: $items)';
 
   @override
-  bool operator ==(Object other) {
+  bool operator ==(covariant Cart other) {
     if (identical(this, other)) return true;
 
-    return other is Cart && mapEquals(other.items, items);
+    return mapEquals(other.items, items);
   }
 
   @override
